45e499618b05f0e27f74afba57c8a6e6a503142c
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2026-02-21 15:48:08 +0300

    removed unnecessary scrips files

--- BDD/2026-02-16-fitia-3.sql
+++
@@ -1,5 +0,0 @@
-create view donationlibcpl_vivre as
-select * from donationlibcpl where idcategorie='CAT0004';
-
-create view donationlibcpl_nonvivre as
-select * from donationlibcpl where idcategorie='CAT0005';
